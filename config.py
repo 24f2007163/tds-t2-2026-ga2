6b835d402403c39d2a362733d81893557b2f63c6
24f2007163 <24f2007163@ds.study.iitm.ac.in>
2026-07-06 12:25:44 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+EMAIL = "24f2007163@ds.study.iitm.ac.in"
+
+Q1_ALLOWED_ORIGIN = "https://dash-bbpad6.example.com"
+
+ISSUER = https://idp.exam.local"
+AUDIENCE = "tds-v0wsq391.apps.exam.local"
+PUBLIC_KEY_PEM= """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
+EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
+WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
+ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
+SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
+-----END PUBLIC KEY-----"""
+
+Q3_PORT = 8032
+Q3_WORKERS = 8
+Q3_DEBUG = False
+Q3_LOG_LEVEL = "info"
+
+Q5_API_KEY = "ak_kfiocqkdk4nes3bu5qpccl04"
+
+Q9_TOTAL_ORDERS = 57
+Q9_RATE_LIMIT = 18
+
+Q10_ALLOWED_ORIGIN = "https://app-nb8404.example.com"
+Q10_RATE_LIMIT = 15
+
+EXAM_PORTAL_ORIGIN = "https://exam.sanand.workers.dev"
